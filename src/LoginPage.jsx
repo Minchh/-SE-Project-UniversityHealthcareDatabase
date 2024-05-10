b5a008c10c875f988e826afce70c0890ea837e46
Minch <minh.nguyendo3103@gmail.com>
2024-05-10 22:55:55 +0700

    login page #1

---
+++ src/LoginPage.jsx
@@ -0,0 +1,14 @@
+import FormContainer from "./components/FormContainer";
+import Slider from "./components/Slider";
+import LoginForm from "./components/LoginForm"
+
+const LoginPage = () => {
+  return (
+    <FormContainer>
+      <Slider />
+      <LoginForm />
+    </FormContainer>
+  );
+};
+
+export default LoginPage;
